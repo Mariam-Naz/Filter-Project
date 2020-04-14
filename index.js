bd785a6fd2f1c2182075235c52a7f838e7751cbb
unknown <mariamnaz212@gmail.com>
2020-04-14 19:09:34 +0500

    update the project

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ document.querySelectorAll("button")[1].addEventListener('click',function(){
         document.querySelectorAll('img')[i].classList.add('hide');
     }
     var cak = ['./cakes/cake-1.jpg','./cakes/cake-2.jpg','./cakes/cake-3.jpg','./cakes/cake-4.jpg'];
-    for(var i = 0;i<=3;i++){
-        document.querySelectorAll('img')[i].setAttribute('src',cak[i]);
-    }
+    
+        for(var i = 0;i<=3;i++){
+            document.querySelectorAll('img')[i].setAttribute('src',cak[i]);
+        }
 });
 
 document.querySelectorAll("button")[2].addEventListener('click',function(){
@@ -24,9 +25,10 @@ document.querySelectorAll("button")[2].addEventListener('click',function(){
         document.querySelectorAll('img')[i].classList.add('hide');
     }
     var bis = ['./buiscuits/biscuit-1.jpg','./buiscuits/biscuit-2.jpg','./buiscuits/biscuit-3.jpg','./buiscuits/biscuit-4.jpg'];
-    for(var i = 0;i<=3;i++){
-        document.querySelectorAll('img')[i].setAttribute('src',bis[i]);
-    }
+    setTimeout(function(){ 
+        for(var i = 0;i<=3;i++){
+            document.querySelectorAll('img')[i].setAttribute('src',bis[i]);
+        }},400);
 });
 
 document.querySelectorAll("button")[3].addEventListener('click',function(){
@@ -34,7 +36,8 @@ document.querySelectorAll("button")[3].addEventListener('click',function(){
         document.querySelectorAll('img')[i].classList.add('hide');
     }
     var ice = ['./ice_cream/ice-cream-1.jpg','./ice_cream/ice-cream-2.jpg','./ice_cream/ice-cream-3.jpg','./ice_cream/ice-cream-4.jpg'];
+    setTimeout(function(){ 
     for(var i = 0;i<=3;i++){
         document.querySelectorAll('img')[i].setAttribute('src',ice[i]);
-    }
+    }},400);
 });
